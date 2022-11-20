772d453713caae96d408d3e448ae6af91995d238
Harish1651999 <hk1651999@gmail.com>
2022-11-20 18:07:01 +0530

    add tapping context with useContext hook.

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,30 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../store/auth-context";
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            {ctx.isLoggedIn && (
-              <ul>
-                <li>
-                  <a href="/">Users</a>
-                </li>
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              </ul>
-            )}
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      {ctx.isLoggedIn && (
+        <ul>
+          <li>
+            <a href="/">Users</a>
+          </li>
+          <li>
+            <a href="/">Admin</a>
+          </li>
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        </ul>
+      )}
+    </nav>
   );
 };
 
